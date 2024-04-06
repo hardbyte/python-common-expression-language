f98b4111385a83d7e44aa4ef7624100c86d1fdfc
Brian Thorne <brian@thorne.link>
2024-04-06 23:23:47 +1300

    ✨ Start of a CEL package

---
+++ src/lib.rs
@@ -0,0 +1,214 @@
+use cel_interpreter::objects::{Key, TryIntoValue};
+use cel_interpreter::{Context, Program, Value};
+use log::debug;
+use pyo3::exceptions::PyValueError;
+use pyo3::prelude::*;
+use pyo3::types::{PyDict, PyList};
+use std::collections::HashMap;
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug)]
+struct RustyCelType(Value);
+
+impl IntoPy<PyObject> for RustyCelType {
+    fn into_py(self, py: Python<'_>) -> PyObject {
+        // Just use the native rust type's existing
+        // IntoPy implementation
+        match self {
+            // Primitive Types
+            RustyCelType(Value::Null) => py.None(),
+            RustyCelType(Value::Bool(b)) => b.into_py(py),
+            RustyCelType(Value::Int(i64)) => i64.into_py(py),
+            RustyCelType(Value::UInt(u64)) => u64.into_py(py),
+            RustyCelType(Value::Float(f)) => f.into_py(py),
+            RustyCelType(Value::Timestamp(ts)) => ts.into_py(py),
+            RustyCelType(Value::String(arcStr)) => arcStr.as_ref().into_py(py),
+            RustyCelType(Value::List(val)) => {
+                let list = val
+                    .as_ref()
+                    .into_iter()
+                    .map(|v| RustyCelType(v.clone()).into_py(py))
+                    .collect::<Vec<PyObject>>();
+                list.into_py(py)
+            }
+            RustyCelType(Value::Bytes(val)) => {
+                let bytes = val;
+                bytes.as_ref().as_slice().into_py(py)
+            }
+            RustyCelType(Value::Duration(d)) => d.into_py(py),
+
+            RustyCelType(Value::Map(val)) => {
+                // Create a PyDict with the converted Python key and values.
+                let python_dict = PyDict::new_bound(py);
+
+                val.map.as_ref().into_iter().for_each(|(k, v)| {
+                    // Key is an enum with String, Uint, Int and Bool variants. Value is any RustyCelType
+                    let key = match k {
+                        Key::String(arcStr) => arcStr.as_ref().into_py(py),
+                        Key::Uint(u64) => u64.into_py(py),
+                        Key::Int(i64) => i64.into_py(py),
+                        Key::Bool(b) => b.into_py(py),
+                        _ => panic!("Invalid key type in Map"),
+                    };
+                    let value = RustyCelType(v.clone()).into_py(py);
+                    python_dict
+                        .set_item(key, value)
+                        .expect("Failed to set item in Python dict");
+                });
+
+                python_dict.into()
+            }
+
+            // Turn everything else into a String:
+            nonprimitive => format!("{:?}", nonprimitive).into_py(py),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct RustyPyType<'a>(&'a PyAny);
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum CelError {
+    ConversionError(String)
+}
+
+impl fmt::Display for CelError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Cel Error")
+    }
+}
+impl Error for CelError {}
+
+/// We can't implement TryIntoValue for PyAny, so we implement for our wrapper type
+impl TryIntoValue for RustyPyType<'_> {
+    type Error = CelError;
+
+    fn try_into_value(self) -> Result<Value, Self::Error> {
+        let val = match self {
+            RustyPyType(pyobject) => {
+                if let Ok(value) = pyobject.extract::<i64>() {
+                    Ok(Value::Int(value))
+                } else if let Ok(value) = pyobject.extract::<f64>() {
+                    Ok(Value::Float(value))
+                } else if let Ok(value) = pyobject.extract::<bool>() {
+                    Ok(Value::Bool(value))
+                } else if let Ok(value) = pyobject.extract::<String>() {
+                    Ok(Value::String(value.into()))
+
+                // TODO Deal with container types (List, Dict etc)
+
+                    // } else if let Ok(value) = pyobject.extract::<PyList>() {
+                //     let list = value
+                //         .iter()
+                //         .map(|item| RustyPyType((*item)).try_into_value().expect("Failed to convert PyList to Value"))
+                //         .collect::<Vec<Value>>();
+                //     Ok(Value::List(list.into()))
+                // } else if let Ok(value) = pyobject.extract::<PyDict>() {
+                //     let mut map:HashMap<Key, Value> = HashMap::new();
+                //     for (key, value) in value.into_iter() {
+                //         let key = key.extract::<String>()?;
+                //
+                //         map.insert(Key::String(key.into()), RustyPyType(*value).try_into_value().expect("Failed to convert PyDict to Value"));
+                //     }
+                //     Ok(Value::Map(map.into()))
+                } else {
+                    Err(CelError::ConversionError("Failed to convert PyAny to Value".to_string()))
+                }
+            }
+        };
+        val
+
+    }
+}
+
+/// Evaluate a CEL expression
+/// Returns a String representation of the result
+#[pyfunction]
+fn evaluate(src: String, context: Option<&PyDict>) -> PyResult<RustyCelType> {
+    debug!("Evaluating CEL expression: {}", src);
+    debug!("Context: {:?}", context);
+
+    let program = Program::compile(src.as_str());
+
+    // Handle the result of the compilation
+    match program {
+        Err(compile_error) => {
+            println!("An error occurred during compilation");
+            println!("compile_error: {:?}", compile_error);
+            // compile_error
+            //     .into_iter()
+            //     .for_each(|e| println!("Parse error: {:?}", e));
+            return Err(PyValueError::new_err("Parse Error"));
+        }
+        Ok(program) => {
+            let mut environment = Context::default();
+            environment.add_function("add", |a: i64, b: i64| a + b);
+
+            // Add any variables from the passed in Dict context
+            if let Some(context) = context {
+                for (key, value) in context {
+                    let key = key.extract::<String>().unwrap();
+                    // Each value is of type PyAny, we need to try to extract into a Value
+                    // and then add it to the CEL context
+
+
+                    let wrapped_value = RustyPyType(value);
+                    match wrapped_value.try_into_value() {
+                        Ok(value) => {
+                            environment
+                                .add_variable(key, value)
+                                .expect("Failed to add variable to context");
+                        }
+                        Err(error) => {
+                            println!("An error occurred during conversion");
+                            println!("Conversion error: {:?}", error);
+                            return Err(PyValueError::new_err("Conversion Error"));
+                        }
+                    }
+
+                    // This direct way is a bit hacky, need to find a better way to do this with traits
+                    // if let Ok(value) = value.extract::<i64>() {
+                    //     environment
+                    //         .add_variable(key, Value::Int(value))
+                    //         .expect("Failed to add variable to context");
+                    // } else if let Ok(value) = value.extract::<f64>() {
+                    //     environment
+                    //         .add_variable(key, value)
+                    //         .expect("Failed to add variable to context");
+                    // } else if let Ok(value) = value.extract::<bool>() {
+                    //     environment
+                    //         .add_variable(key, value)
+                    //         .expect("Failed to add variable to context");
+                    // } else if let Ok(value) = value.extract::<String>() {
+                    //     environment
+                    //         .add_variable(key, value)
+                    //         .expect("Failed to add variable to context");
+                    // }
+                }
+            }
+
+            let result = program.execute(&environment);
+            match result {
+                Err(error) => {
+                    println!("An error occurred during execution");
+                    println!("Execution error: {:?}", error);
+                    // errors
+                    //     .into_iter()
+                    //     .for_each(|e| println!("Execution error: {:?}", e));
+                    Err(PyValueError::new_err("Execution Error"))
+                }
+
+                Ok(value) => return Ok(RustyCelType(value)),
+            }
+        }
+    }
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn cel(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(evaluate, m)?)?;
+    Ok(())
+}
